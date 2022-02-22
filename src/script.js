a97914dc8cd2d79cee4a7a9080fc39c5a183c876
unknown <victoria.prjt@gmail.com>
2022-02-22 11:18:02 -0300

    fixed other player stats

--- src/script.js
+++ src/script.js
@@ -51,11 +51,11 @@ $(".imgThree").click(function () {
 //Inicia o jogo após seleção de personagens
 function startGame() {
     if( caracter == "Warrior"){
-        player = new Players("Chesterfield", 28, 16, 2, { "name": "lance", "mod": { "toHit": 20, "toDamage": 4 }, "toHit": 20, "damage": [1, 6] });
+        player = new Players("Chesterfield", 28, 16, 2, { "name": "lance", "mod": { "toHit": 6, "toDamage": 4 }, "toHit": 20, "damage": [1, 6] });
     } else if( caracter == "Mage"){
-        player = new Players("Chesterfield", 22, 12, 3, { "name": "fire", "mod": { "toHit": 8, "toDamage": 6 }, "toHit": 20, "damage": [1, 8] });
+        player = new Players("Chesterfield", 34, 12, 6, { "name": "fire", "mod": { "toHit": 8, "toDamage": 6 }, "toHit": 20, "damage": [1, 10] });
     } else{
-        player = new Players("Chesterfield", 34, 13, 6, { "name": "sword", "mod": { "toHit": 8, "toDamage": 6 }, "toHit": 20, "damage": [1, 8] });
+        player = new Players("Chesterfield", 22, 13, 2, { "name": "sword", "mod": { "toHit": 10, "toDamage": 5 }, "toHit": 20, "damage": [1, 4] });
     } 
     if (document.getElementById("name").value != "") player.name = document.getElementById("name").value;
     
